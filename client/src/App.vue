ffd4546c42c1b68c719e0e1553399c9160ff2450
Shawn Tschoepe <shawn.42496@gmail.com>
2020-05-03 12:17:27 -0500

    Add link to brand name

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">Auth For Newbs</a>
+      <router-link class="navbar-brand" :to="{ name: 'Home' }">Auth For Newbs</router-link>
     </nav>
     <router-view class="container pt-5"/>
   </div>
