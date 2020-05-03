40dc34bf7d0ba209d7bfcbc61358341cc0be1387
Shawn Tschoepe <shawn.42496@gmail.com>
2020-05-03 12:14:54 -0500

    Add Signup page

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <a class="navbar-brand" href="#">Auth For Newbs</a>
     </nav>
-    <router-view class="container"/>
+    <router-view class="container pt-5"/>
   </div>
 </template>
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <div class="jumbotron">
-      <h1 class="display-3">Hello, world!</h1>
+    <div class="jumbotron text-center">
+      <h1 class="display-3">Auth For Newbs</h1>
       <p class="lead">
-        This is a simple hero unit, a simple jumbotron-style component for calling extra attention
-        to featured content or information.
+        Learning Auth is fun!
       </p>
       <hr class="my-4" />
       <p>
-        It uses utility classes for typography and spacing to space content out within the larger
-        container.
+        Sign Up Here
       </p>
       <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <router-link class="btn btn-primary btn-lg" :to="{ name: 'Signup' }" role="button"
+          >Sign Up</router-link
+        >
       </p>
     </div>
   </div>
