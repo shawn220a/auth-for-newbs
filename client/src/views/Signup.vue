4318049150536aad81b4ac802b141c4306419e18
Shawn Tschoepe <shawn.42496@gmail.com>
2020-05-03 16:15:58 -0500

    Add Signup form

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    <h1>Hello World!</h1>
+    <h1>Sign Up</h1>
+    <!-- Sign Up Form -->
+    <form>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input
+          type="username"
+          class="form-control"
+          id="username"
+          aria-describedby="usernameHelp"
+          placeholder="Enter a username"
+        />
+        <small id="usernameHelp" class="form-text text-muted"
+          >Username Min of 2 characters and Max is 30 characters. Can include (0-9, a-z, A-Z)</small
+        >
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          placeholder="Password"
+        />
+        <small id="passwordHelp" class="form-text text-muted"
+          >Password must be a minimum of 8 charcters.</small
+        >
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
