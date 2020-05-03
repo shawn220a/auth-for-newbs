67fdc2323af9c4dbceb6b01b4d906b2557242829
Shawn Tschoepe <shawn.42496@gmail.com>
2020-05-03 17:47:13 -0500

    Signup validation complete front end

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,40 +1,120 @@
 <template>
   <div>
     <h1>Sign Up</h1>
+
+    <!-- Error Message -->
+    <div v-if="errorMessage" class="alert alert-danger" role="alert">
+      {{ errorMessage }}
+    </div>
+
     <!-- Sign Up Form -->
-    <form>
+    <form @submit.prevent="signup">
       <div class="form-group">
         <label for="username">Username</label>
         <input
+          v-model="user.username"
           type="username"
           class="form-control"
           id="username"
           aria-describedby="usernameHelp"
           placeholder="Enter a username"
+          required
         />
-        <small id="usernameHelp" class="form-text text-muted"
-          >Username Min of 2 characters and Max is 30 characters. Can include (0-9, a-z, A-Z)</small
-        >
+        <small id="usernameHelp" class="form-text text-muted">
+          Username minimum of 2 characters and maximum of 30 characters. Can
+          include (0-9,a-z,A-Z,_).
+        </small>
       </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          placeholder="Password"
-        />
-        <small id="passwordHelp" class="form-text text-muted"
-          >Password must be a minimum of 8 charcters.</small
-        >
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="password">Password</label>
+          <input
+            v-model="user.password"
+            type="password"
+            class="form-control"
+            id="password"
+            aria-describedby="passwordHelp"
+            placeholder="Password"
+            required
+          />
+          <small id="passwordHelp" class="form-text text-muted"
+            >Password must be a minimum of 8 charcters.</small
+          >
+        </div>
+        <div class="form-group col-md-6">
+          <label for="confirmPassword">Confirm Password</label>
+          <input
+            v-model="user.confirmPassword"
+            type="password"
+            class="form-control"
+            id="confirmPassword"
+            aria-describedby="confirmPasswordHelp"
+            placeholder="Password"
+            required
+          />
+          <small id="confirmPasswordHelp" class="form-text text-muted">
+            Please confirm your password.
+          </small>
+        </div>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Sign Up</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+import Joi from '@hapi/joi';
+
+const schema = Joi.object({
+  username: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9_]{2,30}$'))
+    .required(),
+  password: Joi.string()
+    .trim()
+    .min(8)
+    .required(),
+  confirmPassword: Joi.string()
+    .trim()
+    .min(8)
+    .required(),
+});
+
+export default {
+  data: () => ({
+    errorMessage: '',
+    user: {
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+  }),
+  methods: {
+    signup() {
+      if (this.validUser()) {
+        // Send data to server
+      }
+    },
+    validUser() {
+      if (this.user.password !== this.user.confirmPassword) {
+        this.errorMessage = 'Passwords must match.';
+        return false;
+      }
+
+      const result = schema.validate(this.user);
+
+      if (result.error === null) {
+        return true;
+      }
+
+      if (result.error.message.includes('username')) {
+        this.errorMessage = 'Username is invalid';
+      } else {
+        this.errorMessage = 'Password is invalid';
+      }
+      return false;
+    },
+  },
+};
 </script>
 
 <style></style>
