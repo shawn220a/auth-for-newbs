e35ce937ba309a1afb9f426061cef6e185782a93
Shawn Tschoepe <shawn.42496@gmail.com>
2020-05-04 00:37:19 -0500

    Create login middleware

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,8 +3,25 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Signup from '../views/Signup.vue';
 import Login from '../views/Login.vue';
+import Dashboard from '../views/Dashboard.vue';
 
 Vue.use(VueRouter);
+
+function loggedInRedirect(to, from, next) {
+  if (localStorage.token) {
+    next('/dashboard');
+  } else {
+    next();
+  }
+}
+
+function isLoggedIn(to, from, next) {
+  if (localStorage.token) {
+    next();
+  } else {
+    next('/login');
+  }
+}
 
 const routes = [
   {
@@ -16,11 +33,19 @@ const routes = [
     path: '/signup',
     name: 'Signup',
     component: Signup,
+    beforeEnter: loggedInRedirect,
   },
   {
     path: '/login',
     name: 'Login',
     component: Login,
+    beforeEnter: loggedInRedirect,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    beforeEnter: isLoggedIn,
   },
 ];
 
